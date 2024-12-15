728a3026280888d302f9d166df244387716906fa
Kurniawati Agustina <113650883+KurniawatiAgustina@users.noreply.github.com>
2024-12-15 13:00:00 +0700

    table pesanan

---
+++ database/migrations/2025_01_10_044520_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('branch_id');
+            $table->integer('total_price');
+            $table->enum('status', ['New', 'Processing', 'Delivered', 'Cancelled'])->default('New');
+            $table->enum('category', ['AntarJemput', 'AntarSaja', 'JemputSaja', 'Mandiri']);
+            $table->enum('payment_method', ['Cash', 'Transfer']);
+            $table->unsignedBigInteger('payment_method_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
