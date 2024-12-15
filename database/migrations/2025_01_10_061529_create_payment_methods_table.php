5a58bf41963fbc150d87c65e13324ae009fed36c
Kurniawati Agustina <113650883+KurniawatiAgustina@users.noreply.github.com>
2024-12-15 13:00:00 +0700

    table metode pembayaran

---
+++ database/migrations/2025_01_10_061529_create_payment_methods_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('branch_id');
+            $table->string('bank_name');
+            $table->string('account_name');
+            $table->string('account_number');
+            $table->enum('bank_type', ['Bank', 'E-Wallet']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+};
