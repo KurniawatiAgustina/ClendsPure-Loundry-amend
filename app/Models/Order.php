8ed9539f8ec67e28e8013e69dc6eaa05bb5c52cd
Kurniawati Agustina <113650883+KurniawatiAgustina@users.noreply.github.com>
2024-12-20 13:00:00 +0700

    model customer dan pesanan

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Scopes\RoleScope;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope(new RoleScope);
+    }
+
+    public function details()
+    {
+        return $this->hasMany(OrderDetail::class);
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+}
