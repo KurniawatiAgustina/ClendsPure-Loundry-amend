a7c7da1b300fc2c86e9e4d7997dc9c90980b0f34
Kurniawati Agustina <113650883+KurniawatiAgustina@users.noreply.github.com>
2025-01-03 13:00:00 +0700

    pembuatan controller cabang

---
+++ app/Http/Controllers/BranchController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Branch;
+use Illuminate\Http\Request;
+
+class BranchController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $branch = Branch::all();
+        return view('pages.dashboard.branch.index', compact('branch'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+        ], [
+            'name.required' => 'Nama harus diisi',
+            'address.required' => 'Alamat harus diisi',
+            'phone.required' => 'No Telepon harus diisi',
+        ]);
+
+        $branch = Branch::create($validated);
+
+        return redirect()->back()->with('toast_success', 'Branch created successfully');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'address' => 'required',
+            'phone' => 'required',
+        ], [
+            'name.required' => 'Nama harus diisi',
+            'address.required' => 'Alamat harus diisi',
+            'ohone.required' => 'No Telepon harus diisi',
+        ]);
+
+        $branch = Branch::findOrFail($id);
+        $branch->update($validated);
+
+        return redirect()->back()->with('toast_success', 'Branch updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $branch = Branch::findOrFail($id);
+        $branch->delete();
+
+        return redirect()->back()->with('toast_success', 'Branch deleted successfully');
+    }
+}
