c63c6ffa8ebb3ffe79135f988d0e1c1a7a14d54f
Kurniawati Agustina <113650883+KurniawatiAgustina@users.noreply.github.com>
2025-04-08 13:00:00 +0700

    filter dan data dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,25 +18,15 @@ class DashboardController extends Controller
      */
     public function index(Request $request)
     {
-        $userCount = User::count();
-        $branchCount = Branch::count();
-        $customerCount = Customer::count();
-        $sumIncome = Order::sum('total_price');
-        $transactionCount = Order::count();
+        if ($request->start_date && $request->end_date) {
+            $startDate = Carbon::createFromFormat('m/d/Y', $request->start_date)->startOfDay()->toDateTimeString();
+            $endDate = Carbon::createFromFormat('m/d/Y', $request->end_date)->endOfDay()->toDateTimeString();
 
-        $rows = Order::whereMonth('created_at', date('m') - 1)
-            ->whereYear('created_at', date('Y'))
+            $chartData = Order::whereBetween('created_at', [$startDate, $endDate])
             ->selectRaw('DATE(created_at) as full_date, SUM(total_price) as total_income')
             ->groupBy('full_date')
-            ->orderBy('full_date', 'asc')
-            ->get();
+            ->orderBy('full_date', 'asc');
 
-        $chartData = $rows->map(function($row) {
-            return [
-                'date'         => Carbon::parse($row->full_date)->format('j'),
-                'total_income' => $row->total_income,
-            ];
-        });
         $topServices = Service::select(
                 'services.id',
                 'services.name',
@@ -44,21 +34,78 @@ class DashboardController extends Controller
             )
             ->join('order_details', 'services.id', '=', 'order_details.service_id')
             ->join('orders', 'order_details.order_id', '=', 'orders.id')
-            ->whereMonth('orders.created_at', date('m') - 1)
+            ->whereBetween('orders.created_at', [$startDate, $endDate])
             ->groupBy('services.id', 'services.name')
             ->orderByDesc('total_ordered')
-            ->limit(5)
-            ->get();
+            ->limit(5);
 
-        // dd([
-        //     'userCount' => $userCount,
-        //     'branchCount' => $branchCount,
-        //     'customerCount' => $customerCount,
-        //     'sumIncome' => $sumIncome,
-        //     'transactionCount' => $transactionCount,
-        //     'chartData' => $chartData,
-        //     'topServices' => $topServices,
-        // ]);
-        return view('pages.dashboard.general.dashboard');
+            $userCount = User::count();
+            $branchCount = Branch::count();
+            $customerCount = Customer::count();
+            $sumIncome = Order::sum('total_price');
+            $sumIncomeThisMonth = Order::whereMonth('created_at', date('m'))->sum('total_price');
+            $transactionCount = Order::count();
+
+            $data = (object)[
+                'userCount' => $userCount,
+                'branchCount' => $branchCount,
+                'customerCount' => $customerCount,
+                'sumIncome' => $sumIncome,
+                'sumIncomeThisMonth' => $sumIncomeThisMonth,
+                'transactionCount' => $transactionCount,
+                'chartDataKey' => $chartData->pluck('full_date'),
+                'chartDataValue' => $chartData->pluck('total_income'),
+                'topServicesKey' => $topServices->pluck('name'),
+                'topServicesValue' => $topServices->pluck('total_ordered'),
+                'startDate' => Carbon::parse($startDate)->locale('id_ID')->translatedFormat('j F Y'),
+                'endDate' => Carbon::parse($endDate)->locale('id_ID')->translatedFormat('j F Y'),
+                'startDateUnformatted' => $request->start_date,
+                'endDateUnformatted' => $request->end_date,
+
+            ];
+
+        } else {
+            $chartData = Order::whereMonth('created_at', date('m'))
+            ->whereYear('created_at', date('Y'))
+            ->selectRaw('DATE(created_at) as full_date, SUM(total_price) as total_income')
+            ->groupBy('full_date')
+            ->orderBy('full_date', 'asc');
+
+        $topServices = Service::select(
+                'services.id',
+                'services.name',
+                DB::raw('SUM(order_details.quantity) as total_ordered')
+            )
+            ->join('order_details', 'services.id', '=', 'order_details.service_id')
+            ->join('orders', 'order_details.order_id', '=', 'orders.id')
+            ->whereMonth('orders.created_at', date('m'))
+            ->whereYear('orders.created_at', date('Y'))
+            ->groupBy('services.id', 'services.name')
+            ->orderByDesc('total_ordered')
+            ->limit(5);
+
+            $userCount = User::count();
+            $branchCount = Branch::count();
+            $customerCount = Customer::count();
+            $sumIncome = Order::sum('total_price');
+            $sumIncomeThisMonth = Order::whereMonth('created_at', date('m'))->sum('total_price');
+            $transactionCount = Order::count();
+
+            $data = (object)[
+                'userCount' => $userCount,
+                'branchCount' => $branchCount,
+                'customerCount' => $customerCount,
+                'sumIncome' => $sumIncome,
+                'sumIncomeThisMonth' => $sumIncomeThisMonth,
+                'transactionCount' => $transactionCount,
+                'chartDataKey' => $chartData->pluck('full_date'),
+                'chartDataValue' => $chartData->pluck('total_income'),
+                'topServicesKey' => $topServices->pluck('name'),
+                'topServicesValue' => $topServices->pluck('total_ordered'),
+                'startDate' => null,
+                'endDate' => null,
+            ];
+        }
+        return view('pages.dashboard.general.dashboard', compact('data'));
     }
 }
--- resources/views/pages/dashboard/general/dashboard.blade.php
+++ resources/views/pages/dashboard/general/dashboard.blade.php
@@ -36,6 +36,66 @@
             </div>
         </div>
 
+        <div
+            class="bg-white border mt-8 dark:border-gray-500 dark:bg-gray-800 shadow-md rounded-lg p-6 w-full  mx-auto col-span-2">
+            <div class="flex justify-between items-center">
+                <h3 class="text-xl dark:text-white font-bold">Filter Grafik Laporan</h3>
+                <form action="" method="get">
+                    @csrf
+                    <div class="flex space-x-2 items-center">
+                        <div class="relative max-w-sm">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path
+                                        d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                                </svg>
+                            </div>
+                            <input id="datepicker-autohide" datepicker datepicker-autohide type="text" name="start_date" value="{{ request('start_date') }}"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-customprimary-500 focus:border-customprimary-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-customprimary-500 dark:focus:border-customprimary-500"
+                                placeholder="Select date">
+                        </div>
+                        <div class="dark:text-white">s/d</div>
+                        <div class="relative max-w-sm">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path
+                                        d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                                </svg>
+                            </div>
+                            <input id="datepicker-autohide" datepicker datepicker-autohide type="text" name="end_date" value="{{ request('end_date') }}"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-customprimary-500 focus:border-customprimary-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-customprimary-500 dark:focus:border-customprimary-500"
+                                placeholder="Select date">
+                        </div>
+                        <div class="relative max-w-sm hidden">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path
+                                        d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                                </svg>
+                            </div>
+                            <input id="datepicker-autohide" datepicker datepicker-autohide type="text"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-customprimary-500 focus:border-customprimary-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-customprimary-500 dark:focus:border-customprimary-500"
+                                placeholder="Select date">
+                        </div>
+                        <button type="submit"
+                            class="text-white bg-customprimary-500 hover:bg-customprimary-700 focus:ring-4 focus:outline-none focus:ring-customprimary-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-customprimary-600 dark:hover:bg-customprimary-700 dark:focus:ring-customprimary-800">
+                            <svg class="w-5 h-5 text-gray-800 text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-width="2"
+                                    d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z" />
+                            </svg>
+
+                            <span class="sr-only">Icon description</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
         <div class="grid w-full grid-cols-1 gap-5 mt-4 lg:grid-cols-3">
             <div
                 class="flex flex-col  items-center justify-center p-4 bg-customprimary-500 border border-gray-200 rounded-lg shadow-md sm:flex-row sm:p-6 dark:border-gray-700 hover:shadow-lg transition-shadow duration-300">
@@ -45,7 +105,7 @@
                 <div class="w-full text-center sm:text-left">
                     <h3 class="text-xl font-bold text-white ">Pengguna</h3>
                     <p class="text-xl font-bold leading-none text-white sm:text-2xl">
-                        32</p>
+                        {{ $data->userCount }} </p>
                 </div>
             </div>
 
@@ -55,9 +115,9 @@
                     <span class="iconify" data-icon="mdi:handshake" data-inline="false"></span>
                 </span>
                 <div class="w-full text-center sm:text-left">
-                    <h3 class="text-xl font-bold text-white ">Mitra</h3>
+                    <h3 class="text-xl font-bold text-white ">Cabang</h3>
                     <p class="text-xl font-bold leading-none text-white sm:text-2xl">
-                        44</p>
+                        {{ $data->branchCount }}</p>
                 </div>
             </div>
 
@@ -69,7 +129,7 @@
                 <div class="w-full text-center sm:text-left">
                     <h3 class="text-xl font-bold text-white ">Customer</h3>
                     <p class="text-xl font-bold leading-none text-white sm:text-2xl">
-                        23</p>
+                        {{ $data->customerCount }}</p>
                 </div>
             </div>
         </div>
@@ -83,7 +143,7 @@
                 <div class="w-full text-center sm:text-left">
                     <h3 class="text-xl font-bold text-white ">Total Pemasukan</h3>
                     <p class="text-xl font-bold leading-none text-white sm:text-2xl">
-                        Rp{{ number_format(230000, 0, ',', '.') }}</p>
+                        Rp {{ number_format($data->sumIncome, 0, ',', '.') }}</p>
                 </div>
             </div>
 
@@ -95,12 +155,12 @@
                 <div class="w-full text-center sm:text-left">
                     <h3 class="text-xl font-bold text-white ">Total Transaksi</h3>
                     <p class="text-xl font-bold leading-none text-white sm:text-2xl">
-                        Rp{{ number_format(40000, 0, ',', '.') }}</p>
+                        {{ $data->transactionCount }}</p>
                 </div>
             </div>
         </div>
 
-        <div
+        {{-- <div
             class="bg-white border mt-8 dark:border-gray-500 dark:bg-gray-800 shadow-md rounded-lg p-6 w-full  mx-auto col-span-2">
             <div class="flex justify-between items-center">
                 <h3 class="text-xl dark:text-white font-bold">Filter Grafik Laporan</h3>
@@ -155,16 +215,20 @@
                     </button>
                 </div>
             </div>
-        </div>
+        </div> --}}
 
         {{-- CHART --}}
         <div class="grid w-full grid-cols-1 gap-5  mt-8 h-full lg:grid-cols-3">
             <div
                 class="bg-white border dark:border-gray-500 dark:bg-gray-800 shadow-md rounded-lg p-6 w-full  mx-auto col-span-2">
                 <div class="flex justify-between items-center mb-3">
-                    <h3 class="text-xl dark:text-white font-bold">Grafik Pemasukan Bulan Ini</h3>
+                    @if ($data->startDate && $data->endDate)
+                        <h3 class="text-xl dark:text-white font-bold">Grafik Pemasukan {{ $data->startDate }} s/d {{ $data->endDate }}</h3>
+                    @else
+                        <h3 class="text-xl dark:text-white font-bold">Grafik Pemasukan Bulan Ini</h3>
+                    @endif
                 </div>
-                <h4 class="text-xl font-bold text-customprimary-600">Rp 5.000.000</h4>
+                <h4 class="text-xl font-bold text-customprimary-600">Rp {{ number_format($data->sumIncomeThisMonth, 0, ',', '.') }}</h4>
                 <p class="text-sm dark:text-white text-gray-500 mb-4">Saldo masuk</p>
                 <div>
                     <canvas id="incomeChart" class="w-full h-60 dark:bg-gray-800"></canvas>
@@ -186,10 +250,10 @@
             new Chart(incomeChartCtx, {
                 type: 'line',
                 data: {
-                    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17','18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31',],
+                    labels: @json($data->chartDataKey),
                     datasets: [{
-                        label: 'Saldo Masuk',
-                        data: [3, 4, 5, 3.5, 6, 7, 5, 6.5, 4.5, 5.5, 3.5, 4, 5, 6, 7, 5, 6.5, 4.5, 5.5, 3.5, 4, 5, 6, 7, 5, 6.5, 4.5, 5.5, 3.5, 4, 5, 6],
+                        label: 'Uang Masuk',
+                        data: @json($data->chartDataValue),
                         borderColor: '#ff9000',
                         backgroundColor: '#ffd756',
                         tension: 0.4,
@@ -234,12 +298,10 @@
             new Chart(topTransaksiChartCtx, {
                 type: 'bar',
                 data: {
-                    labels: ['LB Ambarukmo', 'LB Bantul', 'LB Ambarukmo', 'LB Bantul', 'LB Ambarukmo',
-                        'Outlet Lainnya'
-                    ],
+                    labels: @json($data->topServicesKey),
                     datasets: [{
-                        label: 'Jumlah Outlet',
-                        data: [23, 15, 20, 10, 12, 25],
+                        label: 'Jumlah Terjual',
+                        data: @json($data->topServicesValue),
                         backgroundColor: '#ff9000',
                         borderRadius: 4,
                         barThickness: 20,
